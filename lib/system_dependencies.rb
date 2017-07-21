58ff5e9fb7345820aba74597feb0e48c28cf7951
abdomazzika <newmazzikastyle@gmail.com>
2017-07-21 18:35:44 +0200

    method to gathering all local gems and versions

--- lib/system_dependencies.rb
+++ lib/system_dependencies.rb
@@ -1,6 +1,21 @@
 require 'system_dependencies/version'
-require 'system_dependencies/system_level_libraries'
+require 'rubygems'
 
 module SystemDependencies
-  # Your code goes here...
+  class SystemLibraries
+
+    def self.local_gems
+      packages = []
+      gems = Gem::Specification.sort_by { |g| [g.name.downcase, g.version] }.group_by(&:name)
+
+      gems.map do |gem|
+        name = gem[1][0].name
+        version = gem[1][0].version.as_json['version']
+
+        packages << { name: name, version: version }
+      end
+
+      packages
+    end
+  end
 end
